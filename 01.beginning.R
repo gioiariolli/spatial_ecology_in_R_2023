03db5c5cf457700fec3bac0f7cf286d72be1a69d
Gioia Riolli <146853519+gioiariolli@users.noreply.github.com>
2023-10-03 17:04:08 +0200

    Create 01.beginning.R

---
+++ 01.beginning.R
@@ -0,0 +1,23 @@
+# Under the hashtag the software R will not detect the code, this is a Mark Down project. This is just a comment.
+
+# exercise
+# We will use R as a simple calculator
+2 + 3
+
+# we can assign a value to an object, creating a variable
+a <- 2+ 3
+a
+
+gioia <- 3+5
+gioia
+
+gioia * a
+
+final <- a*gioia
+final
+# in this way the result will be stored under ''final''
+
+final^2 #the result multiply for itself
+
+
+
