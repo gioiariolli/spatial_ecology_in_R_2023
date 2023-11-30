e60538d18920cd3277935054e8512184bacc89aa
Gioia Riolli <146853519+gioiariolli@users.noreply.github.com>
2023-11-30 20:06:46 +0100

    Update 03.communities.multivar.R
    What did we learn?
    -->Basic concept of multivariate analysis;
    -->Ordination method;
    -->To recall the data in a simple manner (decorana function).

--- 03.communities.multivar.R
+++ 03.communities.multivar.R
@@ -1,57 +1,65 @@
-#Two exercises with communities, multivariate analyses
+# We will use vegan package for our multivariate analysis for community ecology.
+# Vegan package is for vegetation analysis.
+# Overlap package is perfect to see how species are interacting during time.
+
 install.packages("overlap")
-library(overlap) #perfect to see how species are interacting in time
-?overlap
+library(overlap) 
+install.packages("vegan")
 library(vegan)
-?vegan #vegan is a community ecology package
 
-#this is also called ordination method or multivariate analysis
 
-data(dune) #dune is the dataset
+# Ordination method or multivariate analysis.
+
+# To recall the data of our dataset "dune" we use the function data.
+
+data(dune) 
 dune
 
-#How to display only the first 6 rows?
-#We use the function head()
+# HOW TO DISPLAY ONLY THE FIRST 6 ROWS OF THE DATASET?
+# By using the function head().
+
 head(dune)
 
-#we will create a MULTIVARIATE ANALYSIS
-#starting using the DECORANA function or Detrended correspondence analysis
-#this is to help how to see the different species are correleted between
-#each other in a plot
-#we take the date and reshape them in a simple manner
+# Starting using the decorana function or "Detrended Correspondence Analysis"
+# It helps to see how the different species are correleted between each other in a plot.
+# We take the date and reshape them in a simple manner.
 
-#lets assign a name to the vegan's library function decorana
+# Lets assign a name to the vegan's library function decorana.
 
- ord <- decorana(dune)
+ord <- decorana(dune)
 ord
 
-#on the console there are shown the values, these are the lenght of the axes
-#so how much long is the range in each axes
+# On the console, among the others, there are shown the values "axis lenghts", 
+# these are the lenght of the 4 axes.
+# So, how long is the range in each axes
 
-#first we wrote the axes lenght, that we get in the console
+# First we wrote the axes lenght
 ldc1 = 3.7004
 ldc2 = 3.1166
 ldc3 = 1.30055
 ldc4 = 1.47888
-#we calculate the total to them
+# We calculate the total to them
 total= ldc1 + ldc2 +ldc3 +ldc4
 
-#see the percentage of each axes
+# Percentage of each axes
 pldc1 = ldc1 *100 /total
 pldc2 = ldc2 *100 /total
 pldc3 = ldc3 *100 /total
 pldc4 = ldc4 *100 /total
-#ets see the values of the percentage
+
+# Lets see the values of the percentage
 pldc1
 pldc2
 pldc3
 pldc4
 
-#lets concentrate on the first two and lets see how much cumulative percentage
-#they can have
+# Lets concentrate on the first two and lets see their cumulative percentage
 pldc1 + pldc2
-#they're representing the 71% of the whole variability
-#final output or plot is 
+
+# They're representing the 71% of the whole variability.
+
+# The final output or plot is 
 plot(ord)
-#the multivariate space is defined by two axes, DCA1 and DCA2
-#the different names are the species, some of them are close others are far away
+
+# The multivariate space is defined by two axes, DCA1 and DCA2
+# the different names are the species, some of them are close others are far away.
