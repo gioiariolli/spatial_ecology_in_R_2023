1af8586c15960c899f3de6742404f205e1107d97
Gioia Riolli <146853519+gioiariolli@users.noreply.github.com>
2024-01-09 17:39:37 +0100

    Create 12.RMarkdown.R

---
+++ 12.RMarkdown.R
@@ -0,0 +1,20 @@
+---
+title: "MarkdownReport"
+author: "Gioia RIolli"
+date: "2024-01-09"
+output: html_document
+---
+
+# My first markdown document
+
+Here you can add some text
+
+```{r, eval= TRUE} 
+2+3
+```
+
+```{r, eval= FALSE} 
+2+3
+```
+# to show both the code and the results
+# if you put eval= false than you only obtain the code without the results
